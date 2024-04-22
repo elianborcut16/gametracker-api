3a780caf5d5b8d728d18efcb9529ec15358be4e3
Elian Borcut <77327525+elianborcut16@users.noreply.github.com>
2024-04-22 16:54:21 +0300

    Update StartupRunner.java

--- src/main/java/com/games/tracker/configuration/StartupRunner.java
+++ src/main/java/com/games/tracker/configuration/StartupRunner.java
@@ -10,7 +10,6 @@ public class StartupRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        // Example startup logic here
         System.out.println("Application has started successfully!");
     }
 }
